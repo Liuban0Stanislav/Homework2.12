f8028c6fc92d2a54e60befdfab8cbe4b2d213998
Liuban0Stanislav <liubanstanislav@yahoo.com>
2023-06-06 14:36:37 +1000

    Проблема с калькулятором

---
+++ src/main/java/com/example/homework2_12/CalculatorService.java
@@ -0,0 +1,19 @@
+package com.example.homework2_12;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public double plus(double num1, double num2) {
+        return num1 + num2;
+    }
+    public double minus(double num1, double num2) {
+        return num1 - num2;
+    }
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+}
